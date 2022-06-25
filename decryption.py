4bd82a943d2b25ef9cb935b73bc658ed864c90a6
KrystianKoszucki <105989632+KrystianKoszucki@users.noreply.github.com>
2022-06-25 21:35:06 +0200

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,19 +1,19 @@
 from cryptography.fernet import Fernet
 
 key= ''
-with open('SekretnyKlucz1.key', 'rb') as file:
+with open('SecretKey.key', 'rb') as file:
     key= file.read()
 
 encrypted_data= ''
-with open('zaszyfrowane_dane.py', 'rb') as file:
+with open('encrypted_data.py', 'rb') as file:
     encrypted_data= file.read()
 
 f= Fernet(key)
 
 decrypted_data= f.decrypt(encrypted_data)
 
-print("Zaszyfrowane dane: ", encrypted_data.decode())
+print("Encrypted data: ", encrypted_data.decode())
 
 print()
 
-print("Odszyfrowane dane: ", decrypted_data.decode())
+print("Decrypted data: ", decrypted_data.decode())
